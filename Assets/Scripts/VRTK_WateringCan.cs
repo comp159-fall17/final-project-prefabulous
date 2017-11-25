8fddaaf54d56f7f810254f08e3fb6404bfea0028
Drew Thompson <d_thompson6@u.pacific.edu>
2017-11-25 13:59:12 -0800

    Added basic watering can test script to show active and inactive range

--- Assets/Scripts/VRTK_WateringCan.cs
+++ Assets/Scripts/VRTK_WateringCan.cs
@@ -10,13 +10,51 @@ public class VRTK_WateringCan : MonoBehaviour {
 	[Tooltip("Material the test can shows during its inactive range")]
 	public Material outOfRangeMaterial;
 
+	[Header("Active Range Variables")]
+	[Tooltip("Angle at which the watering can starts pouring water")]
+	[Range(45f, 75f)]
+	public float activationAngle = 60f;
+
+	MeshRenderer render;
+	float lastAngle;
+
 	// Use this for initialization
 	void Start () {
-		
+
+		render = GetComponent<MeshRenderer> ();
+		lastAngle = transform.eulerAngles.z;
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		float currentAngle = Mathf.Floor (transform.eulerAngles.z);
+
+		// normalize rotation angle in case the can has somehow been flipped multiple times
+		while (currentAngle > 360f) 
+		{
+			currentAngle -= 360f;
+		}
+
+		if (lastAngle != currentAngle)
+		{
+			if (currentAngle > activationAngle && currentAngle < 120f)
+			{
+				if (render.material != inRangeMaterial) 
+				{
+					render.material = inRangeMaterial;
+				}
+			}
+			else
+			{
+				if (render.material != outOfRangeMaterial) 
+				{
+					render.material = outOfRangeMaterial;
+				}
+			}
+			lastAngle = currentAngle;
+		}	
+
 	}
 }
