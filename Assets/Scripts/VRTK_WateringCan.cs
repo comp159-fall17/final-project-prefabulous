d63fb9fac4bc59c63a260c3b20ec77ec01334a19
Drew Thompson <d_thompson6@u.pacific.edu>
2017-11-24 21:53:18 -0800

    Refactored deprecated 3D assets to 'Deprecated' folder - we will create new folders for our new assets

---
+++ Assets/Scripts/VRTK_WateringCan.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRTK_WateringCan : MonoBehaviour {
+
+
+	public Material inRangeMaterial;
+	public Material outOfRangeMaterial;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
