94fe1670d7a1aaeca4eff7377706a93d398b8475
Drew Thompson <d_thompson6@u.pacific.edu>
2017-11-26 13:41:12 -0800

    Child class GrabbeObjectRelayer finds Grabbed Object's name

---
+++ Assets/GrabbedObjectRelayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class GrabbedObjectRelayer : VRTK_InteractGrab {
+
+	GameObject grabbedObject;
+
+	// Use this for initialization
+	void Start () {
+
+		grabbedObject = GetGrabbedObject();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (GetGrabbedObject() != null) Debug.Log (GetGrabbedObject().name);
+
+	}
+}
