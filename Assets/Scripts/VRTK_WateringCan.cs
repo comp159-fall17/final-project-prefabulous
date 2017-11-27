fb975036a4dcfb5ea74e998961592518a9e68e93
khcanniff <k_canniff@u.pacific.edu>
2017-11-26 18:12:32 -0800

    Worked on Watering Can
    Added in a time interval for the watering can. When the interval is done then the water amount decrements.

--- Assets/Scripts/VRTK_WateringCan.cs
+++ Assets/Scripts/VRTK_WateringCan.cs
@@ -1,38 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VRTK_WateringCan : MonoBehaviour {
 
-	[Header("Testing Materials")]
-	[Tooltip("Material the test can shows during its active range")]
-	public Material inRangeMaterial;
-	[Tooltip("Material the test can shows during its inactive range")]
-	public Material outOfRangeMaterial;
+    [Header("Testing Materials")]
+    [Tooltip("Material the test can shows during its active range")]
+    public Material inRangeMaterial;
+    [Tooltip("Material the test can shows during its inactive range")]
+    public Material outOfRangeMaterial;
+    [Tooltip("Text to display remaining amount of water in watering can")]
+    public Text displayWaterAmount;
 
-	[Header("Active Range Variables")]
-	[Tooltip("Angle at which the watering can starts pouring water")]
-	[Range(45f, 75f)]
-	public float activationAngle = 60f;
-	[Tooltip("Integer maximum of water level variable")]
-	[Range(0f, 100f)]
-	public int maximumWaterLevel = 10;
+    [Header("Active Range Variables")]
+    [Tooltip("Angle at which the watering can starts pouring water")]
+    [Range(45f, 75f)]
+    public float activationAngle = 60f;
+    [Tooltip("Integer maximum of water level variable")]
+    [Range(0f, 100f)]
+    public int maximumWaterLevel = 10;
+    [Tooltip("Time interval for watering")]
+    public float timeInterval;
 
-
-	MeshRenderer render;
+    MeshRenderer render;
 	float lastAngle;
 	int waterLevel;
-
+    float timeIntervalStart = 0;
+    bool wateringCanIsActive;
 	// Use this for initialization
 	void Start () {
-
-		render = GetComponent<MeshRenderer> ();
+        render = GetComponent<MeshRenderer> ();
 		lastAngle = transform.eulerAngles.z;
-		waterLevel = 0; // initialize at empty?
-	}
-	
-	// Update is called once per frame
-	void Update () {
+		waterLevel = 10; // initialize at empty?
+        displayWaterAmount.text = "Remaining \n Water " + waterLevel.ToString();
+        wateringCanIsActive = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
 
 		float currentAngle = Mathf.Floor (transform.eulerAngles.z);
 
@@ -49,26 +55,31 @@ public class VRTK_WateringCan : MonoBehaviour {
 				if (render.material != inRangeMaterial) 
 				{
 					render.material = inRangeMaterial;
-				}
+                    wateringCanIsActive = true;
+                }
 			}
 			else
 			{
 				if (render.material != outOfRangeMaterial) 
 				{
 					render.material = outOfRangeMaterial;
-				}
-			}
+                    timeIntervalStart = 0;
+                    wateringCanIsActive = false;
+                }
+            }
 			lastAngle = currentAngle;
-		}	
+		}
+        TimeInterval();
+    }
 
-	}
-
-	void PourWater()
+    //Decrements water, or displays debug log if the can is empty
+    void PourWater()
 	{
 		if (waterLevel != 0)
 		{
 			waterLevel--;
-		} 
+            displayWaterAmount.text = "Remaining \n Water " + waterLevel.ToString();
+        } 
 		else
 		{
 			Debug.Log ("Watering Can is empty");
@@ -95,4 +106,17 @@ public class VRTK_WateringCan : MonoBehaviour {
 		// TODO: Update a physical water level within the can
 	}
 
+    //Counts down the time interval amount. When the amount is under 0 then reset the time interval.
+    void TimeInterval()
+    {
+        if (waterLevel != 0 && wateringCanIsActive)
+        {
+            timeIntervalStart += Time.deltaTime;
+            if(timeIntervalStart >= timeInterval)
+            {
+                PourWater();
+                timeIntervalStart = 0;
+            }
+        }
+    }
 }
