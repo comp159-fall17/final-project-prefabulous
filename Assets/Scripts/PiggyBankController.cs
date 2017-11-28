afa8dbd6a5ee2df65b916a833c02fb05895f472c
Jack Thias <jackthias@icloud.com>
2017-11-27 20:30:29 -0800

    Piggy Bank plays a nice sound when it's picked up.

---
+++ Assets/Scripts/PiggyBankController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.GrabAttachMechanics;
+
+public class PiggyBankController : VRTK_ChildOfControllerGrabAttach {
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    override public bool StartGrab(GameObject grabbingObject, GameObject givenGrabbedObject, Rigidbody givenControllerAttachPoint) {
+        bool result = base.StartGrab(grabbingObject, givenGrabbedObject, givenControllerAttachPoint);
+        GetComponent<AudioSource>().Play();
+        return result;
+    }
+}
