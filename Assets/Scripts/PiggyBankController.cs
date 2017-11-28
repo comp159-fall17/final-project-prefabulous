8b1089c7e981f6011d5c35dc1bad21d6008c58fb
Jack Thias <jackthias@icloud.com>
2017-11-27 21:58:55 -0800

    Added an API for updating money.

--- Assets/Scripts/PiggyBankController.cs
+++ Assets/Scripts/PiggyBankController.cs
@@ -16,9 +16,23 @@ public class PiggyBankController : VRTK_ChildOfControllerGrabAttach {
     [Range(0.01f, 2f)]
     public float fadeSpeed = 0.75f;
 
-    public int money = 0;
+    private int money = 0;
     private Text moneyText;
     Color tempColor;
+
+    public int Money
+    {
+        get
+        {
+            return money;
+        }
+
+        set
+        {
+            money = value;
+            UpdateLabel();
+        }
+    }
 
     // Use this for initialization
     void Start () {
@@ -26,7 +40,7 @@ public class PiggyBankController : VRTK_ChildOfControllerGrabAttach {
             Destroy(this);
         }
         moneyText = moneyLabel.GetComponent<Text>();
-        UpdateMoney(startingMoney);
+        Money = startingMoney;
 	}
 	
 	// Update is called once per frame
@@ -34,6 +48,7 @@ public class PiggyBankController : VRTK_ChildOfControllerGrabAttach {
 		
 	}
 
+    // Called when the Piggy Bank is initially grabbed by the user
     public override bool StartGrab(GameObject grabbingObject, GameObject givenGrabbedObject, Rigidbody givenControllerAttachPoint) {
         bool result = base.StartGrab(grabbingObject, givenGrabbedObject, givenControllerAttachPoint);
         GetComponent<AudioSource>().Play();
@@ -41,16 +56,18 @@ public class PiggyBankController : VRTK_ChildOfControllerGrabAttach {
         return result;
     }
 
+    // Called when the Piggy Bank is initially dropped by the user
     public override void StopGrab(bool applyGrabbingObjectVelocity) {
         base.StopGrab(applyGrabbingObjectVelocity);
         StartCoroutine("FadeTextOut");
     }
 
-    void UpdateMoney(int newValue) {
-        money = newValue;
-        moneyLabel.GetComponent<Text>().text = "$" + String.Format("{0:n}", money);
+    // Updates the label to display the current balance
+    void UpdateLabel() {
+        moneyLabel.GetComponent<Text>().text = "$" + String.Format("{0:n}", Money);
     }
 
+    // Borrowed from ItemDescriptionFader.cs
     IEnumerator FadeTextIn()
     {
         while (moneyText.color.a < 1)
@@ -66,6 +83,7 @@ public class PiggyBankController : VRTK_ChildOfControllerGrabAttach {
         yield return null;
     }
 
+    // Borrowed from ItemDescriptionFader.cs
     IEnumerator FadeTextOut()
     {
         while (moneyText.color.a > 0)
@@ -81,9 +99,20 @@ public class PiggyBankController : VRTK_ChildOfControllerGrabAttach {
         yield return null;
     }
 
+    // Borrowed from ItemDescriptionFader.cs
     void SetTextOpacityTo(float alpha)
     {
         tempColor.a = alpha;
         moneyText.color = tempColor;
+    }
+
+    // Reduces the balance by an amount
+    void Spend(int amount) {
+        Money -= amount;
+    }
+
+    // Increases the balance by an amount
+    void Earn(int amount) {
+        Money += amount;
     }
 }
