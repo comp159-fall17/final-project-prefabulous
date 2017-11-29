a102705f2fb1b7b6168d9202ca9d8f68aac73ef2
Naomi Nunis <n_nunis@u.pacific.edu>
2017-11-28 23:58:41 -0800

    NSync

--- Assets/Scripts/Sun.cs
+++ Assets/Scripts/Sun.cs
@@ -11,7 +11,7 @@ public class Sun : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.RotateAround(Vector3.zero, Vector3.right, 60f * Time.deltaTime); //makes sun rotate around 0 point right axis for rotation
+        transform.RotateAround(Vector3.zero, Vector3.right, 1f * Time.deltaTime); //makes sun rotate around 0 point right axis for rotation
         transform.LookAt(Vector3.zero);
     }
 }
