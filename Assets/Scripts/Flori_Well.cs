78d5dcf32381b24329ad18155bea3046886e371e
khcanniff <k_canniff@u.pacific.edu>
2017-11-30 14:29:46 -0800

    Created a Temp well
    Created and applied the water well and script.

---
+++ Assets/Scripts/Flori_Well.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flori_Well : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Refill Point"))
+        {
+            gameObject.GetComponent<Transform>().position += Vector3.down * Time.deltaTime;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Refill Point"))
+        {
+            //TODO: play a water refill sound
+            gameObject.GetComponent<Transform>().position += Vector3.up * Time.deltaTime;
+        }
+    }
+}
