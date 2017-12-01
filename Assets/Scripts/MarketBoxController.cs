32f87dd1c437fa0107e75bbd3d6cf871bfcffffa
Jack Thias <jackthias@icloud.com>
2017-11-30 19:05:49 -0800

    Added a MarketBoxController

---
+++ Assets/Scripts/MarketBoxController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Controllables.PhysicsBased;
+
+public class MarketBoxController : MonoBehaviour {
+    public GameObject lidObject;
+    public GameObject colliderObject;
+
+    VRTK_PhysicsRotator lid;
+    Collider collider;
+
+	// Use this for initialization
+	void Start () {
+        lid = lidObject.GetComponent<VRTK_PhysicsRotator>();
+        collider = colliderObject.GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void VanishItems() {
+        //GameObject[] inCollision = collider.
+    }
+}
