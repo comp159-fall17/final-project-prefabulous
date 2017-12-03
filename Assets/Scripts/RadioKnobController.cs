598ea3d1335ada6686587111512529d3b376e562
khcanniff <k_canniff@u.pacific.edu>
2017-12-03 14:44:14 -0800

    created radio script shell

---
+++ Assets/Scripts/RadioKnobController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Controllables.PhysicsBased;
+
+public class RadioKnobController : VRTK_PhysicsRotator
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected override float GetNormalizedValue()
+    {
+        Debug.Log("GetNormalizedValue, GetValue() = " + GetValue() + " angleLimits.minimum = " + angleLimits.minimum + " angleLimits.maximum = " + angleLimits.maximum);
+        return VRTK_SharedMethods.NormalizeValue(GetValue(), angleLimits.minimum, angleLimits.maximum);
+    }
+}
