82258b681b22d90611ff5fe99f637f3544712073
Jack Thias <jackthias@icloud.com>
2017-12-04 22:56:02 -0800

    SnapDropZone now overrided to spend money and play sound effect.

--- Assets/Scripts/DispenserController.cs
+++ Assets/Scripts/DispenserController.cs
@@ -15,66 +15,11 @@ public class DispenserController : VRTK_SnapDropZone {
     [Header("Market")]
     [Tooltip("The amount of money it costs to buy this seed")]
     public int sellPrice;
-    [Tooltip("The amount of money refuneded if the seed is returned")]
-    [Range(0f, 1f)]
-    public float refundRatio = 1f;
 
-    //GameObject currentSeed;
-    //private bool grabbed;
-
-    // Use this for initialization
- //   void Start () {
-	//	SpawnSeed();
-	//}
-
-	// Update is called once per frame
-	//protected override void Update () {
- //       base.Update();
- //       if (!grabbed) {
- //           grabbed = SeedWasGrabbed();
- //           if (grabbed)
- //           {
- //               SeedPicked();
- //               PiggyBankController.Instance.Spend(sellPrice);
- //           }
- //       }
- //       if (SeedWasReleased()) {
- //           SeedDropped();
- //           SpawnSeed();
- //       }
-	//}
-
-    //bool SeedWasGrabbed() {
-    //    return currentSeed.transform.parent != gameObject.transform;
-    //}
-
-    //bool SeedWasReleased()
-    //{
-    //    if (grabbed)
-    //    {
-    //        return currentSeed.transform.parent == gameObject.transform;
-    //    }
-    //    return false;
-    //}
-
-    //void SeedPicked() {
-    //    currentSeed.transform.localScale = seedPrefab.transform.localScale;
-    //}
-
-    //void SeedDropped() {
-    //    currentSeed.GetComponent<Rigidbody>().useGravity = true;
-    //    currentSeed.transform.parent = null;
-
-    //}
-
-    //void SpawnSeed() {
-    //    grabbed = false;
-    //    currentSeed = Instantiate(seedPrefab) as GameObject;
-    //    currentSeed.transform.parent = gameObject.transform;
-    //    currentSeed.transform.localScale = Vector3.Scale(currentSeed.transform.localScale,
-    //                                                     seedScalar);
-    //    currentSeed.transform.position = spawnPoint.transform.position;
-    //    currentSeed.GetComponent<Rigidbody>().useGravity = false;
-    //    currentSeed.name = seedPrefab.name;
-    //}
+    public override void OnObjectUnsnappedFromDropZone(SnapDropZoneEventArgs e)
+    {
+        base.OnObjectUnsnappedFromDropZone(e);
+        PiggyBankController.Instance.Spend(sellPrice);
+        GetComponent<AudioSource>().Play();
+    }
 }
