963ce30eff9b765091fc5015d0afa231b629d35e
Jack Thias <jackthias@icloud.com>
2017-12-04 17:01:46 -0800

    Converted Dispenser to a snapdrop zone.

--- Assets/Scripts/DispenserController.cs
+++ Assets/Scripts/DispenserController.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
-public class DispenserController : MonoBehaviour {
+public class DispenserController : VRTK_SnapDropZone {
     [Header("Seed Spawning")]
     [Tooltip("The prefab for the seed that this dispenser will contain")]
     public GameObject seedPrefab;
