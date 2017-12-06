4e95aacb871ab57d810f62280afbddf1d8b0ade2
Drew Thompson <d_thompson6@u.pacific.edu>
2017-12-06 09:58:55 -0800

    Adding chicken video to bulletin. Will replace with a video of seed grabbing maybe?

---
+++ Assets/VideoButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Examples;
+using UnityEngine.Video;
+
+public class VideoButton : ButtonReactor {
+
+	public VideoPlayer videoPlayer;
+
+	protected override void MaxLimitReached (object sender, VRTK.Controllables.ControllableEventArgs e)
+	{
+		base.MaxLimitReached (sender, e);
+		DJOsvaldo.PlayEffectAt ("click", 0.04f);
+		videoPlayer.time = 0d;
+		videoPlayer.Play ();
+	}
+}
