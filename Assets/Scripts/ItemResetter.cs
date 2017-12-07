ec6a2541be180253de2eb8b8dcba2bb507843d64
Drew Thompson <d_thompson6@u.pacific.edu>
2017-12-07 02:48:03 -0800

    Added some models, made a new test scene, updated ItemResetter.cs to include options for resetting to a location or another object's transform. Updated the Landscape prefab by moving floating scenery

--- Assets/Scripts/ItemResetter.cs
+++ Assets/Scripts/ItemResetter.cs
@@ -5,8 +5,22 @@ using UnityEngine;
 public class ItemResetter : MonoBehaviour
 {
 
-	[Tooltip("Time after which this object will reset from touching the ground.")]
+	[Tooltip("Time after which this object will reset after touching the ground.")]
 	public float resetTime = 30f;
+
+	[Header("Direct Position Reset")]
+	[Tooltip("Override the respawn position and use the overridePosition variable instead.")]
+	public bool overrideLocation = false;
+	[Tooltip("Location in global coordinates for this object to respawn at if overrideLocation is selected.")]
+	public Vector3 overridePosition = Vector3.one;
+
+	[Header("GameObject Position Reset")]
+	[Tooltip("Select overrideLocation and this (true) to respawn directly above a GameObject's position in the scene.")]
+	public bool useGameObjectForRespawnLocation = false;
+	[Tooltip("The GameObject which this object will spawn above if both overrideLocation and useGameObjectForRespawnLocation are selected.")]
+	public GameObject gameObjectForRespawn;
+	[Tooltip("Distance above the selected GameObject for this object to respawn at.")]
+	public float dropHeight = 1f;
 
 	Vector3 startingPosition;
 	Vector3 startingRotation;
@@ -18,6 +32,10 @@ public class ItemResetter : MonoBehaviour
 
 		startingPosition = transform.position;
 		startingRotation = transform.eulerAngles;
+		if (name.Contains("Seed"))
+		{
+			gameObjectForRespawn = GameObject.Find ("Seed Bowl");
+		}
 
 	}
 
@@ -31,7 +49,23 @@ public class ItemResetter : MonoBehaviour
 		resetCounter += Time.deltaTime;
 		if (resetCounter >= resetTime)
 		{
-			transform.position = startingPosition;
+			if (!overrideLocation)
+			{
+				transform.position = startingPosition;
+			}
+			else
+			{
+				if (!useGameObjectForRespawnLocation)
+				{
+					transform.position = overridePosition;
+				}
+				else
+				{
+					Vector3 newPosition = gameObjectForRespawn.transform.position;
+					newPosition.y += dropHeight;
+					transform.position = newPosition;
+				}
+			}
 			transform.eulerAngles = startingRotation;
 		}
 	}
