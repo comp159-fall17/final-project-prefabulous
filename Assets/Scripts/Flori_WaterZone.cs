5160757a3922e3b8d7be6897fa2832e69bdf7e58
Jack Thias <jackthias@icloud.com>
2017-12-08 00:48:14 -0800

    Dirt becomes wet when watered.

--- Assets/Scripts/Flori_WaterZone.cs
+++ Assets/Scripts/Flori_WaterZone.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Flori_WaterZone : MonoBehaviour {
+    public GameObject dirt;
+    public Material wetDirt;
+    public float wetTime = 5.0f;
 
-	void OnTriggerEnter(Collider other)
+    Material dryDirt;
+    float timeOfLastWatering;
+
+    private void Start()
+    {
+        dryDirt = dirt.GetComponent<MeshRenderer>().material;
+    }
+
+    private void Update()
+    {
+        DryUpDirt();
+    }
+
+    void OnTriggerEnter(Collider other)
 	{
 		if (!other.CompareTag("Watering Can"))
 		{
 			return;
 		}
 		GetComponentInChildren<Flori_Planter> ().SetCanInRange (true);
+        if (Flori_WateringCan.Instance.CanIsPouring()) {
+            dirt.GetComponent<MeshRenderer>().material = wetDirt;
+            timeOfLastWatering = Time.time;
+        }
 	}
 
 	void OnTriggerExit(Collider other)
@@ -22,4 +42,13 @@ public class Flori_WaterZone : MonoBehaviour {
 		GetComponentInChildren<Flori_Planter> ().SetCanInRange (false);
 	}
 
+    void DryUpDirt() {
+        if (TimeElapsed() > wetTime) {
+            dirt.GetComponent<MeshRenderer>().material = dryDirt;
+        }
+    }
+
+    float TimeElapsed() {
+        return Time.time - timeOfLastWatering;
+    }
 }
