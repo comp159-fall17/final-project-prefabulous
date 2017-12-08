f0bedc1e8f51f527a7d04ce58262e39911ec4796
Drew Thompson <d_thompson6@u.pacific.edu>
2017-12-08 10:13:33 -0800

    Added code to MarketBox to display amount an object is sold for on the box

--- Assets/Scripts/MarketBox.cs
+++ Assets/Scripts/MarketBox.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using VRTK.Controllables.PhysicsBased;
+using UnityEngine.UI;
 
 public class MarketBox : MonoBehaviour {
 	
     [HideInInspector]
     public List<GameObject> inCollision;
 
+	[Header("Text Variables")]
+	[Tooltip("The text that will display the amount of money a disappeared object is sold for.")]
+	public Text moneyAddedText;
+	[Tooltip("The increment between both alpha increments and its fade speed.")]
+	public float fadeIncrement = 0.01f;
+	[Tooltip("How long the text will stay visible without any fading.")]
+	public float textVisiblePeriod = 0.1f;
+
+	CanvasRenderer textRenderer;
+
 	// Use this for initialization
 	void Start () {
         
+		textRenderer = moneyAddedText.gameObject.GetComponent<CanvasRenderer> ();
+		textRenderer.SetAlpha (0f);
+
 	}
 	
 	// Update is called once per frame
@@ -32,6 +46,8 @@ public class MarketBox : MonoBehaviour {
             if (worth != null)
             {
                 PiggyBank.Instance.Earn(worth.worth);
+				moneyAddedText.text = "+$" + worth.worth.ToString();
+				StartCoroutine ("FadeMoneyText");
                 toBeDestroyed.Add(go);
             }
         }
@@ -41,4 +57,24 @@ public class MarketBox : MonoBehaviour {
             Destroy(go);
         }
     }
+
+	IEnumerator FadeMoneyText()
+	{
+		while (textRenderer.GetAlpha() < 1f)
+		{
+			textRenderer.SetAlpha (textRenderer.GetAlpha() + fadeIncrement);
+			yield return new WaitForSeconds (fadeIncrement);
+		}
+		textRenderer.SetAlpha (1f);
+
+		yield return new WaitForSeconds (textVisiblePeriod);
+
+		while (textRenderer.GetAlpha() > 0f)
+		{
+			textRenderer.SetAlpha (textRenderer.GetAlpha() - fadeIncrement);
+			yield return new WaitForSeconds (fadeIncrement);
+		}
+		textRenderer.SetAlpha (0f);
+		yield return null;
+	}
 }
